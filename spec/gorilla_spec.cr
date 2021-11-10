c3ba3545e1d4981fad86005e00fd9ed8849420af
krishpranav <krisna.pranav@gmail.com>
2021-11-10 15:47:50 +0530

    spec: gorilla spec

--- spec/gorilla_spec.cr
+++ spec/gorilla_spec.cr
@@ -1,9 +1,29 @@
 require "./spec_helper"
 
 describe Gorilla do
-  # TODO: Write tests
+  it "runs the spec suite" do
+    
+    Process.run("mkdir", [
+      "bin",
+    ])
 
-  it "works" do
-    false.should eq(true)
+    Process.run("crystal", [
+      "build",
+      "src/gorilla.cr",
+      "--release",
+      "--stats",
+      "-o",
+      "bin/gorilla",
+    ], output: STDOUT, input: STDIN, error: STDERR)
+
+    Process.run("export", [
+      "GORILLADIR=./",
+    ])
+
+    result = Process.run("./bin/gorilla", [
+      "test/main.ch",
+    ], output: STDOUT, input: STDIN, error: STDERR)
+
+    result.exit_status.should eq(0)
   end
 end
