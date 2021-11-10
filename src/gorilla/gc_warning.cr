f5fe4235ad48b4b12a7953b93eb6a6bf282aa949
krishpranav <krisna.pranav@gmail.com>
2021-11-10 16:07:57 +0530

    src: gorilla gc warning

---
+++ src/gorilla/gc_warning.cr
@@ -0,0 +1,8 @@
+lib LibGC
+  alias WarnProc = LibC::Char*, Word ->
+  fun set_warn_proc = GC_set_warn_proc(WarnProc)
+  $warn_proc = GC_current_warn_proc : WarnProc
+end
+
+LibGC.set_warn_proc ->(msg, word) {
+}
